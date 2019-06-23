c7fd7a5939f9fac7fc997d7456ef1af7ed235e3c
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2019-06-23 11:58:04 -0700

    Add untested init, get and set.

--- lib/nebulex_fdb_adapter.ex
+++ lib/nebulex_fdb_adapter.ex
@@ -3,16 +3,89 @@ defmodule NebulexFdbAdapter do
   Documentation for NebulexFdbAdapter.
   """
 
-  @doc """
-  Hello world.
+  # Inherit default transaction implementation
+  use Nebulex.Adapter.Transaction
 
-  ## Examples
+  # Provide Cache Implementation
+  @behaviour Nebulex.Adapter
+  @behaviour Nebulex.Adapter.Queryable
 
-      iex> NebulexFdbAdapter.hello()
-      :world
+  alias Nebulex.Object
 
-  """
-  def hello do
-    :world
+  alias FDB.{Directory, Transaction, Database}
+  alias FDB.Coder.{Subspace}
+
+  ## Adapter
+
+  @impl true
+  defmacro __before_compile__(env) do
+    cache = env.module
+    config = Module.get_attribute(cache, :config)
+    path = Keyword.fetch!(config, :db_path)
+    cluster_file_path = Keyword.fetch!(config, :cluster_file_path)
+
+    quote do
+      def __db_path__, do: unquote(path)
+
+      def __cluster_file_path__, do: unquote(cluster_file_path)
+
+      def __db__ do
+        :ets.lookup_element(:meta, :db, 2)
+      end
+    end
   end
+
+  @impl true
+  def init(opts) do
+    :meta = :ets.new(__MODULE__, [:named_table, :public, {:readd_concurrency, true}])
+    :ok = FDB.start(610)
+    cluster_file_path = Keyword.fetch!(opts, :cluster_file_path)
+    db_path = Keyword.fetch!(opts, :db_path)
+
+    db =
+      FDB.Cluster.create(cluster_file_path)
+      |> FDB.Database.create()
+    root = Directory.new()
+    dir =
+      Database.transact(db, fn tr ->
+        Directory.create_or_open(root, tr, db_path)
+      end)
+    test_dir = Subspace.new(dir)
+    coder = Transaction.Coder.new(test_dir)
+    connected_db = FDB.Database.set_defaults(db, %{coder: coder})
+    true = :ets.insert(__MODULE__, {:db, connected_db})
+    {:ok, []}
+  end
+
+  @impl true
+  def get(_cache, key, opts) do
+    db = Keyword.fetch!(opts, :db)
+    FDB.Database.transact(
+      db,
+      fn transaction ->
+        FDB.Transaction.get(transaction, key)
+      end
+    )
+  end
+
+  @impl true
+  def set(_cache, %Object{key: key, value: value}, opts) do
+    db = Keyword.fetch!(opts, :db)
+    FDB.Database.transact(
+      db,
+      fn transaction ->
+        FDB.Transaction.set(transaction, key, value)
+      end
+    )
+  end
+
+  # FDB.Database.transact(connected_db ,
+  # fn transaction ->
+  #   FDB.Transaction.clear(transaction, "key")
+  # end
+  # )
+
+  # Database.transact(db, fn tr ->
+  #   Directory.list(root, tr, ["nebulex"])
+  # end)
 end
--- mix.exs
+++ mix.exs
@@ -18,10 +18,23 @@ defmodule NebulexFdbAdapter.MixProject do
     ]
   end
 
+  defp nebulex_opts do
+    if System.get_env("NBX_TEST") do
+      [github: "cabol/nebulex", tag: "v1.0.1"]
+    else
+      "~> 1.0"
+    end
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:fdb, "6.0.15-1"},
+      # This is because the adapter tests need some support modules and shared
+      # tests from nebulex dependency, and the hex dependency doesn't include
+      # the test folder. Hence, to run the tests it is necessary to fetch
+      # nebulex dependency directly from GH.
+      {:nebulex, nebulex_opts()},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
